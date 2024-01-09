6598db6a1de3e1962c15b990aed600493a47c962
Agata <a.momot4@student.uw.edu.pl>
2024-01-10 00:21:39 +0100

    Going to adjust action urls in edit and add

--- boniedojade/zachodSlonca/urls.py
+++ boniedojade/zachodSlonca/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.main_site, name="main_site"),
     path("dodaj_trase/", views.dodaj_trase, name="dodaj_trase"),
     path("wyswietl_mape/", views.wyswietl_mape, name="wyswietl_mape"),
-    path("dane_trasy/", views.dane_trasy, name="dane_trasy")
+    path("dane_trasy/", views.dane_trasy, name="dane_trasy"),
+    path("edytuj_trase/", views.edytuj_trase, name="edytuj_trase")
 ]
--- boniedojade/zachodSlonca/views.py
+++ boniedojade/zachodSlonca/views.py
@@ -4,13 +4,17 @@ from django.urls import reverse
 
 
 def main_site(request):
-    return render(request, "zachodSlonca/main_site.html", {})
+    return render(request, "zachodSlonca/main_site.html", {"name" : "Dom-MIM"})
 
 def dodaj_trase(request):
-    return render(request, "zachodSlonca/add_route.html", {})
+    return render(request, "zachodSlonca/add_route.html", {"action" : "Dodaj"})
 
 def dane_trasy(request):
-    return HttpResponseRedirect(reverse("zachodSlonca:main_site"))
+    newName = request.POST.get("uname")
+    return render(request, "zachodSlonca:main_site", {"name" : newName})
+
+def edytuj_trase(request):
+    return render(request, "zachodSlonca/add_route.html", {"action" : "Zapisz"})
 # Create your views here.
 
 def wyswietl_mape(request):
