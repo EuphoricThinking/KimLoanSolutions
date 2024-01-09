776c374ac31461805961273a423e445929478a95
Agata <a.momot4@student.uw.edu.pl>
2024-01-09 23:52:05 +0100

    Templates and views for adding route and basic rendering added, going to add changing name

--- boniedojade/zachodSlonca/urls.py
+++ boniedojade/zachodSlonca/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 app_name = 'zachodSlonca'
 urlpatterns = [
     path("", views.main_site, name="main_site"),
-    path("dodaj_trase/", views.dodaj_trase, name="dodaj_trase")
+    path("dodaj_trase/", views.dodaj_trase, name="dodaj_trase"),
+    path("wyswietl_mape/", views.wyswietl_mape, name="wyswietl_mape"),
+    path("dane_trasy/", views.dane_trasy, name="dane_trasy")
 ]
--- boniedojade/zachodSlonca/views.py
+++ boniedojade/zachodSlonca/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 
 def main_site(request):
     return render(request, "zachodSlonca/main_site.html", {})
 
 def dodaj_trase(request):
-    return HttpResponse("SIALALALALAAAAAAAAAAAA")
+    return render(request, "zachodSlonca/add_route.html", {})
+
+def dane_trasy(request):
+    return HttpResponseRedirect(reverse("zachodSlonca:main_site"))
 # Create your views here.
+
+def wyswietl_mape(request):
+    return HttpResponse("SIALALALALAAAAAAAAAAAA")
