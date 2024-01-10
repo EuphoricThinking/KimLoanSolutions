86069f1f202535c86b353e77f7548dc68d1f96b8
Agata <a.momot4@student.uw.edu.pl>
2024-01-10 07:12:57 +0100

    Added html for show_map

--- boniedojade/zachodSlonca/views.py
+++ boniedojade/zachodSlonca/views.py
@@ -4,18 +4,23 @@ from django.urls import reverse
 
 
 def main_site(request):
-    return render(request, "zachodSlonca/main_site.html", {"name" : "Dom-MIM"})
+    return render(request, "zachodSlonca/main_site.html",
+                  {"name" : "Dom-MIM"})
 
 def dodaj_trase(request):
-    return render(request, "zachodSlonca/add_route.html", {"action" : "Dodaj"})
+    return render(request, "zachodSlonca/add_route.html",
+                  {"action" : "Dodaj"})
 
 def dane_trasy(request):
     newName = request.POST.get("uname")
-    return render(request, "zachodSlonca/main_site.html", {"name" : newName})
+    return render(request, "zachodSlonca/main_site.html",
+                  {"name" : newName})
 
 def edytuj_trase(request):
-    return render(request, "zachodSlonca/add_route.html", {"action" : "Zapisz"})
+    return render(request, "zachodSlonca/add_route.html",
+                  {"action" : "Zapisz"})
 # Create your views here.
 
 def wyswietl_mape(request):
-    return HttpResponse("SIALALALALAAAAAAAAAAAA")
+    return render(request, "zachodSlonca/show_map.html",
+                  {"welcome": "SIALALALALAAAAAAAAAAAA"})
