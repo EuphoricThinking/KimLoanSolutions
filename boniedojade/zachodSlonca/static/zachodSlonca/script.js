5c193212c1543e395ba048092a936a64b5ab9ff9
a.rempala <a.rempala@millionmonkeyssoftware.com>
2024-01-12 11:54:33 +0100

    jedziemy z mapą (pociongiem lub ałtobusem)

--- boniedojade/zachodSlonca/static/zachodSlonca/script.js
+++ boniedojade/zachodSlonca/static/zachodSlonca/script.js
@@ -13,3 +13,67 @@ function hideElem() {
             setTimeout(delayedAction, 3000);
             setTimeout(hideElem, 5000);
         };
+
+const loc = window.location.pathname;
+const dir = loc.substring(0, loc.lastIndexOf('/'));
+const mapa = document.getElementById('mapa')
+const czasPrzejazdu = document.getElementById('czas-przejazdu');
+const opóźnienie = document.getElementById('opóźnienie');
+
+mapa.src = "/static/media/pusta.png";
+
+const godzinaPrzyjazduGłówna = new Date();
+godzinaPrzyjazduGłówna.setHours(13);
+godzinaPrzyjazduGłówna.setMinutes(0);
+
+const godzinaPrzyjazduAlternatywna = new Date();
+godzinaPrzyjazduGłówna.setHours(13);
+godzinaPrzyjazduGłówna.setMinutes(12);
+
+const trasy = {
+    główna: {
+        czas: 36 * 60 * 1000,
+        środki: [['R7', 'pociąg']],
+        godzinaPrzyjazdu: godzinaPrzyjazduGłówna,
+        godzinaDojazdu: godzinaPrzyjazduGłówna + 36 * 60 * 1000,
+        opóźnienie: [40 * 60 * 1000]
+    },
+    alternatywna: {
+        czas: 47 * 60 * 1000,
+        środki: [['521', 'autobus'], ['502', 'autobus'], ['158', 'autobus']],
+        godzinaPrzyjazdu: godzinaPrzyjazduAlternatywna,
+        godzinaDojazdu: godzinaPrzyjazduAlternatywna + 36 * 60 * 1000,
+        opóźnienie: [0, 0, 2 * 60 * 1000]
+    }
+};
+
+const wyswietlGlownaTrase = () => {
+    mapa.src = `/static/media/pociag.png`;
+    czasPrzejazdu.innerHTML = `${trasy.główna.czas / 60 / 1000}min`;
+};
+const wyswietlAlternatywnaTrase = () => {
+    mapa.src = `/static/media/autobus.png`;
+    czasPrzejazdu.innerHTML = `${trasy.alternatywna.czas / 60 / 1000}min`;
+};
+const wyswietlPustaMape = () => {
+    mapa.src = `/static/media/pusta.png`;
+    czasPrzejazdu.innerHTML = "";
+};
+
+document.addEventListener('keydown', (e) => {
+    console.log(e);
+    if (e.shiftKey) {
+        switch (e.code) {
+            case 'KeyQ':
+                wyswietlGlownaTrase();
+                break;
+            case 'KeyW':
+                wyswietlAlternatywnaTrase();
+                break;
+            case 'KeyE':
+                wyswietlPustaMape();
+                break;
+        }
+        console.log(e.code)
+    }
+});
